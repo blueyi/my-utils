7074bb7d216c407cb4d802514624e69cabd98f6f
blueyi <blueyiniu@qq.com>
2017-03-19 09:17:49 +0000

    desktop branch

--- common/misc.cmd
+++ common/misc.cmd
@@ -2,18 +2,19 @@
 #以#开头表示注释，自动忽略#后面的内容
 
 # git configure
-# git config --global user.name "blueyi"
-# git config --global user.email "blueyiniu@qq.com"
+git config --global user.name "blueyi"
+git config --global user.email "blueyiniu@qq.com"
 
 # fonts
 git clone https://github.com/blueyi/myfonts.git ~/.fonts
 sudo fc-cache -f -v
 
 # some useful ppa
-# sudo add-apt-repository -y ppa:wiznote-team/ppa   # wiznote client for ubuntu
+sudo add-apt-repository -y ppa:wiznote-team/ppa   # wiznote client for ubuntu
 # sudo add-apt-repository -y ppa:graphics-drivers/ppa  # nvidia graphic drivers
 # sudo add-apt-repository -y ppa:webupd8team/java  # Oracle JAVA
-# sudo add-apt-repository -y ppa:hzwhuang/ss-qt5  # shadowsocks-qt5 client
+sudo add-apt-repository -y ppa:hzwhuang/ss-qt5  # shadowsocks-qt5 client
+sudo apt-get update
 
 # Another soft
 # http://meetfranz.com/  # Franz support multil messaging app, eg: wechat
@@ -21,12 +22,12 @@ sudo fc-cache -f -v
 # chmod +x app/ss/shadowsocks-local-linux64
 
 # hexo install
-# curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | sh
-# nvm install stable
-# npm install -g hexo-cli
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | sh
+nvm install stable
+npm install -g hexo-cli
 
 # my hexo blog
-# git clone git@github.com:blueyi/hexoblog.git ~/blog
+git clone git@github.com:blueyi/hexoblog.git ~/blog
 
 # Oh My Zsh install
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
