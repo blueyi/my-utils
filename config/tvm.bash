7fe10c427bdb17be924b8a238a4f20824e702e54
blueyi <blueyiniu@qq.com>
2021-12-05 10:42:30 +0800

    update build config and proxy

--- config/tvm.bash
+++ config/tvm.bash
@@ -2,5 +2,6 @@ source $HOME/repos/my-utils/config/resetrc.bash
 export PYTHONPATH=$TVM_HOME/python:$TVM_HOME/topi/python:${PYTHONPATH}
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TVM_HOME/build
 export LIBRARY_PATH=$LD_LIBRARY_PATH
+export TVM_LOG_DEBUG="ir/transform.cc=1;relay/ir/transform.cc=1"
 
 alias ctv='cd ${TVM_HOME}'
