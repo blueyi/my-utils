0753b397013e4c641d204642ae67a88fa5ee6685
blueyi <blueyiniu@qq.com>
2023-11-26 23:03:11 +0800

    update proxy

---
+++ config/dl.bash
@@ -0,0 +1,2 @@
+export MY_CONF_PATH=$HOME/repos/my-utils/config
+source $MY_CONF_PATH/resetrc.bash
--- config/myrc.bash
+++ config/myrc.bash
@@ -1,3 +1,6 @@
 export MY_CONF_PATH=$HOME/repos/my-utils/config
-export TVM_HOME=$HOME/repos/tvm
-source $MY_CONF_PATH/tvm.bash
+# export TVM_HOME=$HOME/repos/tvm
+# source $MY_CONF_PATH/tvm.bash
+
+# source $MY_CONF_PATH/llvm.bash
+source $MY_CONF_PATH/dl.bash
