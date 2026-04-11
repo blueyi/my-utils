4738bcd13790fd4bea186105ea9f6fc202031b6c
yulong <yl.w@outlook.com>
2026-04-11 20:00:02 +0800

    Auto-backup: 2026-04-11 20:00:02 Changed files:  M config/resetrc.bash

--- config/resetrc.bash
+++ config/resetrc.bash
@@ -206,6 +206,7 @@ fi
 _path_add() { [ -d "$1" ] && case ":$PATH:" in *":$1:"*) ;; *) export PATH="$1:$PATH";; esac; }
 [ -n "${CMAKE_PATH:-}" ] && _path_add "$CMAKE_PATH"
 [ -n "${LOCAL_BIN_PATH:-}" ] && _path_add "$LOCAL_BIN_PATH"
+_path_add "$HOME/.local/go/bin"
 [ -n "${MY_BIN:-}" ] && _path_add "$MY_BIN"
 [ -n "${HOME_BIN:-}" ] && _path_add "$HOME_BIN"
 [ -n "${CUDA_BIN_PATH:-}" ] && _path_add "$CUDA_BIN_PATH"
